c5926f7bd51af9b187421fcacde235e03b6121a2
Bbeierle12 <bbeierle21@gmail.com>
2025-08-07 16:56:13 -0700

    h

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -2,6 +2,9 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import Simulator from "./Simulator.jsx"; // the file you pasted into
 
+console.log("index.jsx loaded");
+document.getElementById("root").innerHTML = "<h1>Plain HTML works</h1>";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <Simulator />
