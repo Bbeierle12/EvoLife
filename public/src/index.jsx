25601365d47d01e6e5007deb14bd3a48700408cc
Bbeierle12 <bbeierle21@gmail.com>
2025-08-07 16:48:17 -0700

    Here

--- public/src/index.jsx
+++ public/src/index.jsx
@@ -1,9 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import Simulator from "./Simulator.jsx"; // the file you pasted into
+import EcosystemSimulator from "./Simulator.jsx"; // default export name
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <Simulator />
+    <EcosystemSimulator />
   </React.StrictMode>
 );
